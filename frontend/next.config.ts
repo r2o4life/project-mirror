f77855198c79a074811603ec11507d350d0f61df
Arturo Garcia Art <arrgarci7@gmail.com>
2026-07-14 07:26:56 -0700

    Revert static export for Vercel deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // Note: if deploying to https://[username].github.io/[repo-name]/, you must uncomment and set basePath:
-  // basePath: '/[repo-name]',
+  /* config options here */
 };
 
 export default nextConfig;
